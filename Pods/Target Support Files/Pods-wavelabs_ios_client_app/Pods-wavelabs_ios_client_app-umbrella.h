5f00d206b45fbae7d112228dd704a52dcb8a8fa9
afsara <afsarunnisa@nbostech.com>
2016-03-01 17:16:28 +0530

    deleted lib files

--- Pods/Target Support Files/Pods-wavelabs_ios_client_app/Pods-wavelabs_ios_client_app-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_wavelabs_ios_client_appVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_wavelabs_ios_client_appVersionString[];
-
